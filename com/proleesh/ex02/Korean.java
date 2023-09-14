51dcd1fbfa60fcfe8c44be1a1f9623e8fd80df25
sunghyuklee <merci726@yahoo.com>
2023-09-14 13:57:01 +0900

    9.14 update

--- com/proleesh/ex02/Korean.java
+++ com/proleesh/ex02/Korean.java
@@ -1,10 +1,22 @@
 package com.proleesh.ex02;
 
+/**
+ * 오버로딩을 하는 이유는 매개값으로 객체의 필드를 다양하게 초기화하기 위해서다.
+ */
 public class Korean {
     String nation = "Korea";
     String name;
     String ssn;
 
+    int age;
+
+    public Korean(){
+        System.out.println("default constructor 호출 성공!!");
+    }
+
+    public Korean(int age){
+        this.age = age;
+    }
     public Korean(String name, String ssn){
         this.name = name;
         this.ssn = ssn;
--- com/proleesh/ex02/KoreanExample.java
+++ com/proleesh/ex02/KoreanExample.java
@@ -6,9 +6,28 @@ public class KoreanExample {
         Korean k2 = new Korean("이몽룡", "990102-1000002");
         Korean k3 = new Korean("이순신", "990103-1000003");
 
-        System.out.println("정보: 이름: " + k1.name + ", 주민 번호: " + k1.ssn + ", 국적: " + k1.nation);
-        System.out.println("정보: 이름: " + k2.name + ", 주민 번호: " + k2.ssn + ", 국적: " + k2.nation);
-        System.out.println("정보: 이름: " + k3.name + ", 주민 번호: " + k3.ssn + ", 국적: " + k3.nation);
+        k1.age = 19;
+        System.out.println("정보: 이름: " +
+                k1.name + ", 주민 번호: " + k1.ssn + "," +
+                " 국적: " + k1.nation + ", 나이: " + k1.age);
+        k2.age = 29;
+        System.out.println("정보: 이름: " + k2.name + ", " +
+                "주민 번호: " + k2.ssn + ", " +
+                "국적: " + k2.nation + ", 나이: " + k2.age);
+        k3.age = 49;
+        System.out.println("정보: 이름: " + k3.name +
+                ", 주민 번호: " + k3.ssn + ", 국적: "
+                + k3.nation + ", 나이: " + k3.age);
+
+        Korean k4 = new Korean();
+//        System.out.println(k4.age);
+
+        Korean k5 = new Korean(18);
+        k5.name = "김수로";
+        k5.ssn = "990104-1000004";
+        System.out.println("정보: 이름: " + k5.name +
+                ", 주민 번호: " + k5.ssn + ", 국적: " +
+                k5.nation + ", 나이: " + k5.age);
 
     }
 }
