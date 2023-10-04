1e56c5eb540273f60ac7d8da837f5557197e510a
sunghyuklee <merci726@yahoo.com>
2023-10-04 22:40:51 +0900

    10.4 update

---
+++ com/proleesh/ex07/Singleton.java
@@ -0,0 +1,10 @@
+package com.proleesh.ex07;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton(){}
+    static Singleton getInstance(){
+        return singleton;
+    }
+}
