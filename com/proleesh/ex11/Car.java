bcaab90c0dd2a4214a83be7d8a03dcb09f3b04d2
sunghyuklee <merci726@yahoo.com>
2023-10-09 14:11:59 +0900

    10.9 update

---
+++ com/proleesh/ex11/Car.java
@@ -0,0 +1,13 @@
+package com.proleesh.ex11;
+
+public class Car {
+
+    public int speed;
+    public void speedUp(){
+        speed ++;
+    }
+    public final void stop(){
+        System.out.println("차를 멈춤");
+        speed = 0;
+    }
+}
---
+++ com/proleesh/ex11/SportsCar.java
@@ -0,0 +1,9 @@
+package com.proleesh.ex11;
+
+public class SportsCar extends Car{
+    @Override
+    public void speedUp() {
+        super.speedUp();
+    }
+    // stop 메서드는 final이기 때문에 재정의 불가능
+}
