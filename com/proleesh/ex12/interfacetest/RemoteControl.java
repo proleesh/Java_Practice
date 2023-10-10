90b3921dfbcfae802b341f611c3e9948bae823cb
sunghyuklee <merci726@yahoo.com>
2023-10-10 22:09:27 +0900

    10.10 update

---
+++ com/proleesh/ex12/interfacetest/RemoteControl.java
@@ -0,0 +1,7 @@
+package com.proleesh.ex12.interfacetest;
+
+public interface RemoteControl {
+    // 추상 메서드 abstract 생략 가능
+//    public abstract void turnOn();
+    void turnOn();
+}
---
+++ com/proleesh/ex12/interfacetest/RemoteControlEx.java
@@ -0,0 +1,10 @@
+package com.proleesh.ex12.interfacetest;
+
+public class RemoteControlEx {
+    public static void main(String[] args) {
+//        RemoteControl rc;
+//        rc = new Television();
+        RemoteControl rc = new Television();
+        rc.turnOn();
+    }
+}
