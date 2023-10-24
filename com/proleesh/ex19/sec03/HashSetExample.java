82327b3e87f05f152d0e18678e32d7252ec7b0a6
sunghyuklee <merci726@yahoo.com>
2023-10-24 09:39:11 +0900

    10.24 update

---
+++ com/proleesh/ex19/sec03/HashSetExample.java
@@ -0,0 +1,38 @@
+package com.proleesh.ex19.sec03;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+
+        set.add("Java");
+        set.add("JDBC");
+        set.add("JSP");
+        set.add("Spring");
+
+        // 객체를 하나씩 가져와서 처리
+        Iterator<String> iterator = set.iterator();
+        while(iterator.hasNext()){
+            // 객체를 하나 가져오기
+            String element = iterator.next();
+            System.out.println(element);
+            if(element.equals("JSP")){
+                // 가져온 객체를 컬렉션에서 제거
+                iterator.remove();
+            }
+        }
+        System.out.println();
+
+        // 객체 제거
+        set.remove("JDBC");
+
+        // 객체를 하나씩 가져와서 처리
+        for(String element: set){
+            System.out.println(element);
+        }
+
+    }
+}
