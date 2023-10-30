f3f242c77c7e2e275d8f792e5e0c4a78206ccebe
sunghyuklee <merci726@yahoo.com>
2023-10-30 22:10:12 +0900

    10.30 update

---
+++ com/proleesh/ex20/sec09/Speakable.java
@@ -0,0 +1,5 @@
+package com.proleesh.ex20.sec09;
+@FunctionalInterface
+public interface Speakable {
+    void speak(String content);
+}
---
+++ com/proleesh/ex20/sec09/Workable.java
@@ -0,0 +1,5 @@
+package com.proleesh.ex20.sec09;
+@FunctionalInterface
+public interface Workable {
+    void work(String name, String job);
+}
