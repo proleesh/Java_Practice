e0abe43ba9bf288082a5f9f717f58e938852ea6d
sunghyuklee <merci726@yahoo.com>
2023-10-31 10:44:44 +0900

    10.31 update

---
+++ com/proleesh/ex21/sec04/StreamExample.java
@@ -0,0 +1,26 @@
+package com.proleesh.ex21.sec04;
+
+import java.sql.SQLOutput;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class StreamExample {
+    public static void main(String[] args) {
+        // Set 컬렉션 생성
+        Set<String> set = new HashSet<>();
+        set.add("홍길동");
+        set.add("이순신");
+        set.add("이명박");
+
+        // Stream을 이용한 요소 반복 처리
+        Stream<String> stream = set.stream();
+        stream.forEach(name -> System.out.println(name));
+        /**
+         * Stream은 Iterator와 비슷한 반복자이지만, 다음과 같은 차이점을 가지고 있다.
+         * 1. 내부 반복자이므로 처리 속도가 빠르고 병렬 처리에 효율적이다.
+         * 2. 람다식으로 다양한 요소 처리를 정리할 수 있다.
+         * 3. 중간 처리와 최종 처리를 수행하도록 파이프 라인을 형성할 수 있다.
+         */
+    }
+}
