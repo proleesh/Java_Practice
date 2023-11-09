76ef27e8ba9284b7ba01e4751a36feafad2ad441
sunghyuklee <merci726@yahoo.com>
2023-11-09 09:01:27 +0900

    11.9 update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Java Practice 2023
-### update: 2023.11.8
+### update: 2023.11.9
 ### 비공개 Private Project
 ### 작성자 Author : Sung-Hyuk Lee
---
+++ com/proleesh/ex23/sec02/Student.java
@@ -0,0 +1,19 @@
+package com.proleesh.ex23.sec02;
+
+public class Student {
+    private final String name;
+    private final int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
