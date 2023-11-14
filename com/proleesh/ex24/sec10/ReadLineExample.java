09c88ab85ddda3aab815a5abfabae9b9576879f8
sunghyuklee <merci726@yahoo.com>
2023-11-14 11:11:33 +0900

    11.14 update

--- com/proleesh/ex24/sec10/ReadLineExample.java
+++ com/proleesh/ex24/sec10/ReadLineExample.java
@@ -11,9 +11,9 @@ public class ReadLineExample {
         int lineNo = 1; // 1번 라인 부터
         while(true){
             String str = br.readLine();
-            if(str == null) break;
-            System.out.println(lineNo + "\t" + str);
-            lineNo++;
+            if(str == null) break; // 해당 라인의 내용이 null 이면 중지
+            System.out.println(lineNo + "\t" + str); // 라인 번호 + 해당 라인 내용
+            lineNo++; // 줄 읽기 수 하나 씩 증가
         }
         br.close();
         long end = System.nanoTime();
