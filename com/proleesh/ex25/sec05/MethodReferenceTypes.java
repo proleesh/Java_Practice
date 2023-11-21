a738efd974cd3733da3d9b85349c21fdead2f092
sunghyuklee <merci726@yahoo.com>
2023-11-21 10:15:27 +0900

    11.21 update

--- com/proleesh/ex25/sec05/MethodReferenceTypes.java
+++ com/proleesh/ex25/sec05/MethodReferenceTypes.java
@@ -1,11 +1,14 @@
 package com.proleesh.ex25.sec05;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class MethodReferenceTypes {
     public static void main(String[] args) {
         boundMethodReferences();
+        unboundMethodReferences();
     }
 
 
@@ -22,6 +25,22 @@ public class MethodReferenceTypes {
         Predicate<String> titleMR = name::startsWith;
         System.out.println(titleMR.test("Lee"));
 
+
+    }
+
+    public static void unboundMethodReferences(){
+        Function<String, String> upperL = s->s.toUpperCase();
+        Function<String, String> upperMR = String::toUpperCase;
+
+        System.out.println(upperL.apply("sean"));
+        System.out.println(upperMR.apply("sean"));
+
+        BiFunction<String, String, String> concatL = (s1, s2) -> s1.concat(s2);
+        BiFunction<String, String, String> concatMR = String::concat;
+        System.out.println(concatL.apply("Sean", "Kennedy"));
+
+
+        System.out.println(concatMR.apply("Sean", "Kennedy"));
 
     }
 }
