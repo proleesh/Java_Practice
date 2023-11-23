d4dd5d31d760a85107b268b6c2e51554d6117fe4
sunghyuklee <merci726@yahoo.com>
2023-11-23 09:34:56 +0900

    11.23 update

--- com/proleesh/ex25/sec11/StreamTest.java
+++ com/proleesh/ex25/sec11/StreamTest.java
@@ -1,6 +1,7 @@
 package com.proleesh.ex25.sec11;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamTest {
@@ -8,7 +9,13 @@ public class StreamTest {
         Double[] numbers = {1.1, 2.2, 3.3};
         Stream<Double> stream1 = Arrays.stream(numbers);
 
-        long n = stream1.count();
-        System.out.println("Number of elements: " + n);
+//        long n = stream1.count();
+        System.out.println("Number of elements: " + stream1.count());
+
+        List<String> animalList = Arrays.asList("cat", "dog", "sheep");
+        Stream<String> streamAnimal = animalList.stream();
+        System.out.println("Number of elements: " + streamAnimal.count());
+
+
     }
 }
