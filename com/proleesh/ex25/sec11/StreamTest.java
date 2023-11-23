3905b8eb613733bcf422bd547232812f2161f827
sunghyuklee <merci726@yahoo.com>
2023-11-23 09:38:41 +0900

    11.23 update

--- com/proleesh/ex25/sec11/StreamTest.java
+++ com/proleesh/ex25/sec11/StreamTest.java
@@ -1,7 +1,9 @@
 package com.proleesh.ex25.sec11;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class StreamTest {
@@ -16,6 +18,15 @@ public class StreamTest {
         Stream<String> streamAnimal = animalList.stream();
         System.out.println("Number of elements: " + streamAnimal.count());
 
+        Map<String, Integer> namesToAges = new HashMap<>();
+        namesToAges.put("Mike", 22);
+        namesToAges.put("Mary", 24);
+        namesToAges.put("Alice", 31);
+        System.out.println("Number of entries: " +
+                namesToAges
+                .entrySet()
+                .stream()
+                .count());
 
     }
 }
