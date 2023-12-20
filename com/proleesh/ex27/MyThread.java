41316766bc8e58698b12ee59c2d7ab33393efbc3
Sung-Hyuk Lee <merci726@yahoo.com>
2023-12-20 16:16:11 +0900

    12.20.2023 Changes

---
+++ com/proleesh/ex27/MyThread.java
@@ -0,0 +1,13 @@
+package com.proleesh.ex27;
+
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        System.out.println("run(): " + getName());
+    }
+
+    public static void main(String[] args) {
+        new MyThread().start();
+        System.out.println("main(): " + Thread.currentThread().getName());
+    }
+}
