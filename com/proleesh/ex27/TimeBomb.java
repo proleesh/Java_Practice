f3bc0fa789342c13a62b84f9ad9f5407244e73c1
Sung-Hyuk Lee <merci726@yahoo.com>
2023-12-21 18:46:02 +0900

    12.21.2023 Changes

---
+++ com/proleesh/ex27/TimeBomb.java
@@ -0,0 +1,23 @@
+package com.proleesh.ex27;
+
+class CountDown implements Runnable{
+    String[] timeStr = {
+            "Zero", "One", "Two", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine"
+    };
+
+    @Override
+    public void run() {
+        for(int i = 9; i >= 0; --i){
+            System.out.println(timeStr[i]);
+        }
+    }
+}
+public class TimeBomb {
+    public static void main(String[] args) {
+        Thread timer = new Thread(new CountDown());
+        System.out.println("Starting 10 seconds count down... ");
+        timer.start();
+        System.out.println("Boom!!!");
+    }
+}
