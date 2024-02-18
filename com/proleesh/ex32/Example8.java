758259569ec2d7b8322eb35827a9ca0402436f53
sunghyuklee <merci726@yahoo.com>
2024-02-18 21:32:02 +0900

    2024.2.18 update

--- com/proleesh/ex32/Example8.java
+++ com/proleesh/ex32/Example8.java
@@ -9,21 +9,21 @@ import java.util.StringTokenizer;
 
 public class Example8{
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader =
-                new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int inputNum = Integer.parseInt(stringTokenizer.nextToken());
-        int quizNum = Integer.parseInt(stringTokenizer.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int inputNum = Integer.parseInt(st.nextToken());
+        int quizNum = Integer.parseInt(st.nextToken());
         long[] S = new long[inputNum + 1];
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        st  = new StringTokenizer(br.readLine());
         for(int i = 1; i <= inputNum; ++i){
-            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
         }
 
         for(int q = 0; q < quizNum; ++q){
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int j = Integer.parseInt(stringTokenizer.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
             System.out.println(S[j] - S[i - 1]);
         }
 
