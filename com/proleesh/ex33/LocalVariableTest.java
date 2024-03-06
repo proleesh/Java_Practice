c5dee087ae28cce9ec265557de874dfc67f40ea3
sunghyuklee <merci726@yahoo.com>
2024-03-06 17:00:55 +0900

    2024.3.6 update

---
+++ com/proleesh/ex33/LocalVariableTest.java
@@ -0,0 +1,11 @@
+package com.proleesh.ex33;
+
+public class LocalVariableTest {
+    public static void main(String[] args) {
+        var x = 2;
+        var y = 3.2;
+        var t = "abc";
+
+
+    }
+}
---
+++ com/proleesh/ex33/LocalVariableTypeInterface.java
@@ -0,0 +1,12 @@
+package com.proleesh.ex33;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocalVariableTypeInterface {
+    public static void main(String[] args) {
+        var s = "Hello world";
+
+        System.out.println(s);
+    }
+}
