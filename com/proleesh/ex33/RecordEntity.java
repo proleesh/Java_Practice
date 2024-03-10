5fb6a372bd7c94b385fb3180437f946afb022dc1
sunghyuklee <merci726@yahoo.com>
2024-03-10 14:25:55 +0900

    2024.3.10 update

--- com/proleesh/ex33/RecordEntity.java
+++ com/proleesh/ex33/RecordEntity.java
@@ -1,4 +1,14 @@
 package com.proleesh.ex33;
 
-public record RecordEntity(Long id, String name, Integer age) {
+public record RecordEntity(Long id, String name, Integer age, String regNumber) {
+
+    public static final String currentYear = "24";
+
+    public boolean isNewEntity(){
+        return regNumber().substring(0, 2).equals(currentYear);
+    }
+
+    public static RecordEntity createBlankRecordEntity(){
+        return new RecordEntity(2L, " ", 24, " ");
+    }
 }
--- com/proleesh/ex33/RecordTest.java
+++ com/proleesh/ex33/RecordTest.java
@@ -7,7 +7,7 @@ public class RecordTest {
     public static void main(String[] args) {
         AtomicLong atomicLong = new AtomicLong(1L);
 
-        RecordEntity re = new RecordEntity(atomicLong.getAndAdd(1L), "홍길동", 18);
+        RecordEntity re = new RecordEntity(atomicLong.getAndAdd(1L), "홍길동", 18, "A0001S");
 
         String string = re.toString();
         System.out.println(string);
@@ -18,6 +18,15 @@ public class RecordTest {
         System.out.println(re.age());
         System.out.println("~~~~~~~~~~~");
 
+        System.out.println(re.isNewEntity());
+
+        System.out.println(RecordEntity.currentYear);
+
+
+        RecordEntity blankEntity = RecordEntity.createBlankRecordEntity();
+        System.out.println("regNumber: " + blankEntity.regNumber());
+        System.out.println("id: " + blankEntity.id());
+        System.out.println("name: " + blankEntity.name());
 
     }
 }
