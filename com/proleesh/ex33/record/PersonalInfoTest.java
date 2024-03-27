c9603e8d3c24e1b2d765e0360feb58e96e6d6509
sunghyuklee <merci726@yahoo.com>
2024-03-28 08:10:17 +0900

    2024.3.28 update

--- com/proleesh/ex33/record/PersonalInfoTest.java
+++ com/proleesh/ex33/record/PersonalInfoTest.java
@@ -1,7 +1,9 @@
 package com.proleesh.ex33.record;
 
+import java.lang.reflect.RecordComponent;
+
 public class PersonalInfoTest {
-    public static void main(String[] args) {
+    void main() {
         Address addr = new Address("Seoul", "Seoul, South Korea", "01000");
         PhoneNumber pn = new PhoneNumber("010-1234-5678");
         Customer customer = new Customer(1, "Janne Kim", pn.number(), addr.address());
@@ -12,5 +14,12 @@ public class PersonalInfoTest {
         System.out.println(customer);
         System.out.println(customer.info());
 
+        boolean record = customer.getClass().isRecord();
+        System.out.println(record);
+
+        RecordComponent[] recordComponents = customer.getClass().getRecordComponents();
+        for(RecordComponent recordComponent : recordComponents){
+            System.out.println(STR."recordComponent = \{recordComponent}");
+        }
     }
 }
