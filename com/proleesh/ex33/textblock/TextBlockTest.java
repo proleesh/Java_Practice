e2fcf2a201338d73533918fc9606415ed9ce5b75
sunghyuklee <merci726@yahoo.com>
2024-04-04 21:33:08 +0900

    2024.4.4 update about TextBlock

---
+++ com/proleesh/ex33/textblock/TextBlockTest.java
@@ -0,0 +1,17 @@
+package com.proleesh.ex33.textblock;
+
+public class TextBlockTest {
+    void main(){
+        String s1 = """
+                Hello world
+                """;
+        System.out.println(s1);
+
+        String s2 = """
+                Great job!,
+                What's your favorite job?
+                Programmer
+                So Good!""";
+        System.out.println(s2);
+    }
+}
