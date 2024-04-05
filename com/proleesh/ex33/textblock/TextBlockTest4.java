338fceee0681e873bdedcb18a3a2c0a472844181
sunghyuklee <merci726@yahoo.com>
2024-04-05 10:19:22 +0900

    2024.4.5 update about TextBlock

---
+++ com/proleesh/ex33/textblock/TextBlockTest4.java
@@ -0,0 +1,12 @@
+package com.proleesh.ex33.textblock;
+
+public class TextBlockTest4 {
+    void main(){
+        String str = """
+                Spring Boot is developer framework to fast \
+                based on \" Spring \" Framework, created Spring application \
+                embedded web server, using jar or war running \
+                """;
+        System.out.println(str);
+    }
+}
