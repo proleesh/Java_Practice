c90c31d37af70525a93638cf5be35b562f36f353
sunghyuklee <merci726@yahoo.com>
2024-05-12 23:23:16 +0900

    2024.5.12 update

---
+++ com/proleesh/ex34/primitivearray/PrimitiveArrayTest.java
@@ -0,0 +1,13 @@
+package com.proleesh.ex34.primitivearray;
+
+public class PrimitiveArrayTest {
+    public static void main(String[] args) {
+        /**
+         * 모든 primitive type의 값이나 array, java 객체는 stack이 아니라 heap에 저장한다,
+         * 반대로 모든 지역 변수는 stack에 저장한다.
+         */
+        int[] iArr;
+        iArr = new int[]{2, 5, -12, 20};
+
+    }
+}
---
+++ com/proleesh/ex34/primitivearray/PrimitiveArrayTest2.java
@@ -0,0 +1,10 @@
+package com.proleesh.ex34.primitivearray;
+
+public class PrimitiveArrayTest2 {
+    public static void main(String[] args) {
+        int[] iArr = null;
+        System.out.println(iArr);
+        iArr = new int[5];
+        System.out.println(iArr.length);
+    }
+}
