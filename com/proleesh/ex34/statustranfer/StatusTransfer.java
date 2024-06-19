f29d1e7976a1d06d236ec2db01dd207945a67add
sunghyuklee <merci726@yahoo.com>
2024-06-19 15:37:52 +0900

    update 6.19

---
+++ com/proleesh/ex34/statustranfer/StatusTransfer.java
@@ -0,0 +1,12 @@
+package com.proleesh.ex34.statustranfer;
+
+public class StatusTransfer {
+    public static void test(){
+        String a = new String("자바 마스터");
+        a = new String("스프링 부트");
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
