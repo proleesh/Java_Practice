5dfbde85994ce7436a6577dd5950cfce6ee9f094
sunghyuklee <merci726@yahoo.com>
2024-08-18 19:53:36 +0900

    update 8.18

---
+++ com/proleesh/ex34/UglyNum.java
@@ -0,0 +1,33 @@
+package com.proleesh.ex34;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
+public class UglyNum {
+    public static void main(String[] args) {
+        UglyNum uglyNum = new UglyNum();
+        System.out.println(uglyNum.nthUglyNumber(10));
+    }
+    public int nthUglyNumber(int n) {
+        int[] primes = {2, 3, 5};
+        PriorityQueue<Long> uglyHeap = new PriorityQueue<>();
+        HashSet<Long> visited = new HashSet<>();
+
+        uglyHeap.add(1L);
+        visited.add(1L);
+
+        long curr = 1L;
+        for(int i = 0; i < n; ++i){
+            curr = uglyHeap.poll();
+            for(int prime : primes){
+                long new_ugly = curr * prime;
+                if(!visited.contains(new_ugly)){
+                    uglyHeap.add(new_ugly);
+                    visited.add(new_ugly);
+                }
+            }
+        }
+        return (int)curr;
+
+    }
+}
