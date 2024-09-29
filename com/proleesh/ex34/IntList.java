dd9fe6d41cbc5b01ba3fe67fafe493c40a0122d9
proleesh <merci726@yahoo.com>
2024-09-30 08:00:13 +0900

    update 9.30

---
+++ com/proleesh/ex34/IntList.java
@@ -0,0 +1,24 @@
+package com.proleesh.ex34;
+
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r){
+        first = f;
+        rest = r;
+    }
+
+    public static void main(String[] args) {
+//        IntList list = new IntList();
+//        list.first = 5;
+//        list.rest = null;
+//
+//        list.rest = new IntList();
+//        list.rest.first = 10;
+//
+//        list.rest.rest = new IntList();
+//        list.rest.rest.first = 15;
+
+    }
+}
